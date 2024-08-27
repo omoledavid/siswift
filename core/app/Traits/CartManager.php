797080db6934c98bf2146f7b092640bc33740774
Your Name <you@example.com>
2024-08-27 15:54:56 +0100

    update

--- core/app/Traits/CartManager.php
+++ core/app/Traits/CartManager.php
@@ -168,25 +168,26 @@ trait CartManager
 
     public function getCartItems($request)
     {
-        $user_id    = auth()->user()->id;
-        if ($user_id != null) {
-            $data = Cart::where('user_id', $user_id)->with(['product', 'product.stocks', 'product.categories', 'product.offer'])
-                ->whereHas('product', function ($q) {
-                    return $q->whereHas('categories')->whereHas('brand');
-                })
-                ->orderBy('id', 'desc')
-                ->get();
-        } else {
-            $s_id       = session()->get('session_id');
-            $data = Cart::where('session_id', $s_id)
-                ->with(['product', 'product.stocks', 'product.categories',])
-                ->whereHas('product', function ($q) {
-                    return $q->whereHas('categories')->whereHas('brand');
-                })
-                ->orderBy('id', 'desc')
-                ->get();
-        }
-        return $data;
+        $user    = auth()->user();
+
+//        if ($user_id != null) {
+//            $data = Cart::where('user_id', $user_id)->with(['product', 'product.stocks', 'product.categories'])
+//                ->whereHas('product', function ($q) {
+//                    return $q->whereHas('categories')->whereHas('brand');
+//                })
+//                ->orderBy('id', 'desc')
+//                ->get();
+//        } else {
+//            $s_id       = session()->get('session_id');
+//            $data = Cart::where('session_id', $s_id)
+//                ->with(['product', 'product.stocks', 'product.categories',])
+//                ->whereHas('product', function ($q) {
+//                    return $q->whereHas('categories')->whereHas('brand');
+//                })
+//                ->orderBy('id', 'desc')
+//                ->get();
+//        }
+        return  Cart::query()->where('user_id', $user->id)->with('product')->orderBy('id', 'desc')->get();
     }
 
     public function deleteCartItem($id)
