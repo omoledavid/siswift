4b0651991adeea9da563ad708793691b7883def0
Your Name <you@example.com>
2024-09-03 00:14:40 +0100

    update

--- core/app/Models/Cart.php
+++ core/app/Models/Cart.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
     protected $guarded  = ['id'];
     protected $casts    = ['attributes'=>'array'];
 
-    public function product()
+    public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class, 'product_id');
     }
